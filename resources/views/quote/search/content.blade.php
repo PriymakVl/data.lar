261bc8a873b6229903d2df03b044ef468592ca87
unknown <C:\Users\\AppData\Roaming\The Bat!\Mail>
2019-11-01 16:57:22 +0200

    01-11-19-work

--- resources/views/quote/search/content.blade.php
+++ resources/views/quote/search/content.blade.php
@@ -3,7 +3,7 @@
   @include('layouts.messages')
   
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2">
-    <h1 class="h2">Результаты поиска</h1>
+    <h1 class="h2">Результаты поиска по "{{ $search }}"</h1>
   </div>
 
 	<table class="table table-bordered text-center">
